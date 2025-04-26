ed1e7ae1576cc31f7ad97de3d621e5dc3b63d3ce
JonBabao <jonendrickb@gmail.com>
2025-04-26 22:02:04 +0800

    Add UI/UX for Register.tsx and Implement Google and LinkedIn API

--- src/app/components/Register.tsx
+++ src/app/components/Register.tsx
@@ -3,6 +3,12 @@
 import React, { useState } from 'react';
 import { createClient } from '../../../lib/supabase/client';
 import Link from "next/link";
+import AuthImage from "../../../public/images/authimage.png";
+import Logo from "../../../public/images/logoBig.png"
+import OrangeButton from '../styles/orangeButton';
+import GoogleSignInButton from './GoogleSignIn';
+import LinkedInSignInButton from './LinkedInSignIn'
+import Blob from "../../../public/images/blobRegister.png"
 
 const Register: React.FC = () => {
     const supabase = createClient();
@@ -53,52 +59,104 @@ const Register: React.FC = () => {
         setSuccess("Registration successful! Check your email for confirmation.");
     };
 
+    const handleGoogleLogin = async () => {
+        try {
+          const { error } = await supabase.auth.signInWithOAuth({
+            provider: 'google',
+            options: {
+              redirectTo: `${window.location.origin}/auth/callback`
+            }
+          })
+    
+          if (error) throw error
+        } catch (error) {
+          console.error('Error signing in with Google:', error)
+          alert('Error signing in with Google')
+        }
+      }
+
     return(
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Username"
-                required
-            />
-            <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email Address"
-                required
-            />
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-                required
-            />
-            <input
-                type="password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                placeholder="Confirm Password"
-                required
+        <div className="flex flex-row">
+            <img 
+                src={AuthImage.src}
+                alt="Auth Image"
+                className="h-screen w-[50vw]"
             />
 
-            {error && <p className="text-red-500">{error}</p>}
-            {success && <p className="text-green-500">{success}</p>}
+            <section className="flex flex-col items-center px-36 text-center w-full">
+                <img
+                    src={Logo.src}
+                    alt="Logo"
+                    className="self-start -ml-36"
+                />
+                <h2 className="montserrat text-5xl font-bold">Join the SWAP Community</h2>
+                <p className="my-4">Let's turn skill into shared success.</p>
 
-            <button type="submit">
-                Sign Up
-            </button>
-            <div>
-                <p>Have an account already?&nbsp;</p>
-                <Link href="/auth/login">
-                    <button type="button">
-                        Login
-                    </button>
-                </Link>
-            </div>
-        </form>
+                <img 
+                    src={Blob.src}
+                    className="absolute right-0 scale-75 -mr-12 mt-22"
+                />
+
+
+                <form onSubmit={handleSubmit} className="flex flex-col">
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="Full Name"
+                        className="w-96 px-4 py-2 mb-2 border-1 border-gray-300 rounded-lg"
+                        required
+                    />
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Email Address"
+                        className="w-96 px-4 py-2 mb-2 border-1 border-gray-300 rounded-lg"
+                        required
+                    />
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Password"
+                        className="w-96 px-4 py-2 mb-2 border-1 border-gray-300 rounded-lg"
+                        required
+                    />
+                    <input
+                        type="password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        placeholder="Confirm Password"
+                        className="w-96 px-4 py-2 mb-2 border-1 border-gray-300 rounded-lg"
+                        required
+                    />
+
+                    {error && <p className="text-red-500">{error}</p>}
+                    {success && <p className="text-green-500">{success}</p>}
+
+                    <OrangeButton type="submit" style={{ height: "3rem"}}>Create Account</OrangeButton>
+                </form>
+                <div className="flex flex-row my-4 items-center">
+                    <hr className="w-38" />
+                    <p className="px-6">OR</p>
+                    <hr className="w-38" />
+                </div>
+                <div className="space-y-2">
+                    <GoogleSignInButton />
+                    <LinkedInSignInButton />
+                </div>
+                
+                <p className="mt-4">Already have an account?&nbsp; 
+                    <Link href="/auth/login" className="text-blue-700">
+                        Log In
+                    </Link>
+                </p>
+                
+            </section>
+            
+        </div>
+        
     );
 };
 
