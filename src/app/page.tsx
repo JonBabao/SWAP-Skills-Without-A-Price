0263576dcc15d66ba977f851b2e5a7afa386f52d
JonBabao <jonendrickb@gmail.com>
2025-04-28 21:47:29 +0800

    Rename Home.tsx to LandingPage.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Home from "../app/components/Home";
+import LandingPage from "./components/LandingPage";
 import TopNav from "../app/components/TopNav";
 
 export default function LoginPage() {
     return(
         <div>
             <TopNav />
-            <Home />
+            <LandingPage />
         </div>
     );
 }
