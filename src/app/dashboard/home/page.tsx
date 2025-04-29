0b77dc3a373da30423c7783af11018d831d307b6
JonBabao <jonendrickb@gmail.com>
2025-04-29 22:24:26 +0800

    Create page.tsx for /dashboard/home

---
+++ src/app/dashboard/home/page.tsx
@@ -0,0 +1,10 @@
+import Home from '../../components/Home'
+import MainLayout from '@/app/components/layout/MainLayout'
+
+export default function HomePage() {
+    return(
+       <MainLayout>
+            <Home />
+       </MainLayout>
+    );
+}
