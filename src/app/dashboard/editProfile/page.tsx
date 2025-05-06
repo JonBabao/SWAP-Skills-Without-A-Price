428a91175d359f33eca8522cb1165d6380074e15
chustinecantal <22-06033@g.batstate-u.edu.ph>
2025-05-06 08:37:53 +0800

    Add Edit Profile

---
+++ src/app/dashboard/editProfile/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import EditProfilPagee from "@/app/components/EditProfile";
+import MainLayout from "@/app/components/layout/MainLayout";
+
+export default function EditProfile() {
+    return(
+        <MainLayout>
+            <EditProfilPagee />
+        </MainLayout>
+    );
+}
