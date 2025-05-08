75f39d0280243c8a653814e7c8dfbb47ba6b14b3
chustinecantal <22-06033@g.batstate-u.edu.ph>
2025-05-08 10:28:24 +0800

    Updating the header part in EditProfile

--- src/app/components/EditProfile.tsx
+++ src/app/components/EditProfile.tsx
@@ -1,12 +1,16 @@
 "use client";
 import React, { useState, useEffect } from 'react';
+import logo from '../../../public/images/logo.png';
+
 
 
 const EditProfile: React.FC = () => {
     return(
-        <section>
-            <p>Helloworld</p>
-        </section>
+        <div className="seashell w-[1920px] h-[112px] relative">
+           <img src="/images/logo.png" className="absolute left-[43px] top-[28px] w-16 h-[55px]" />
+        </div>
+
+      
     );
 };
 
