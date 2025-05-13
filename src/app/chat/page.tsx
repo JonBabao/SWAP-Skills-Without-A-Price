5bcfe9344a83f006c745a57c007984d88b958816
JonBabao <jonendrickb@gmail.com>
2025-05-13 23:35:28 +0800

    Create Chat.tsx and corresponding directory

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Chat from "@/app/components/Chat";
+import MainLayout from "@/app/components/layout/MainLayout";
+
+export default function EditProfile() {
+    return(
+        <MainLayout>
+            <Chat />
+        </MainLayout>
+    );
+}
