ae9a9c91064b06ee5186c57efb42724716cd6eff
JonBabao <jonendrickb@gmail.com>
2025-05-15 04:24:24 +0800

    Refactor code

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import HomePage from "../components/HomePage";
+import MainLayout from "@/app/components/layout/MainLayout";
+
+export default function Home() {
+    return(
+        <MainLayout>
+            <HomePage />
+        </MainLayout>
+    );
+}
