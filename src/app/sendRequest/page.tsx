7588efb6ee4d22a578c6c4f23ab470f4a1247cd2
JonBabao <jonendrickb@gmail.com>
2025-05-14 23:47:14 +0800

    Create page.tsx and sendRequest directory for SendRequest.tsx component

---
+++ src/app/sendRequest/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import SendRequest from "../components/SendRequest";
+import MainLayout from "@/app/components/layout/MainLayout";
+
+export default function EditProfile() {
+    return(
+        <MainLayout>
+            <SendRequest />
+        </MainLayout>
+    );
+}
